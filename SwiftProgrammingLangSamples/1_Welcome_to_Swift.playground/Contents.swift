d633de1db05f3dc83d2f9ba6b11e9e7e133d34f2
Adam Chomicki <admchm@outlook.com>
2022-06-28 09:05:58 +0200

    Update playground with the first samples from the book

--- SwiftProgrammingLangSamples/1_Welcome_to_Swift.playground/Contents.swift
+++ SwiftProgrammingLangSamples/1_Welcome_to_Swift.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/* A Swift Tour */
+print("Hello world")
+
+// Simple Values
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+
+let implicitInteger = 70
+let imiplicitDouble = 70.0
+let explicitDouble: Double = 70
