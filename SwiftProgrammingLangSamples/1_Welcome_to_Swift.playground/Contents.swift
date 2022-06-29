5b8bb72e835a4e67f0168efab4b818575ab1ad0d
Adam Chomicki <admchm@outlook.com>
2022-06-29 21:51:16 +0200

    Finish "simple values" section

--- SwiftProgrammingLangSamples/1_Welcome_to_Swift.playground/Contents.swift
+++ SwiftProgrammingLangSamples/1_Welcome_to_Swift.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 /* A Swift Tour */
 print("Hello world")
 
-// Simple Values
+/* SIMPLE VALUES */
 var myVariable = 42
 myVariable = 50
 let myConstant = 42
@@ -11,3 +11,54 @@ let myConstant = 42
 let implicitInteger = 70
 let imiplicitDouble = 70.0
 let explicitDouble: Double = 70
+
+/* EXPERIMENT:
+ * Create a constant with an explicit type of Float and a value of 4
+ */
+let explicitFloat: Float = 4
+type(of: explicitFloat)
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+/* EXPERIMENT:
+ * Try removing the conversion to String from the last line. What error do you get?
+ */
+// let widthLabel = label + width - ERROR: Binary operator '+' cannot be applied to operands of type 'String' and 'Int'
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+/* EXPERIMENT:
+ * Use \() to include a floating-point calculation in a string and to include someone's name in a greeting.
+ */
+let yourName = "Adam"
+let appleDevicesCount: Float = 3.5
+let greetingWithCalc = "Hello \(yourName)! We want to remind you that you have \(appleDevicesCount) Apple devices - there is a half, because one of them is broken!"
+
+let quotation = """
+I said "I have \(apples) apples."
+And then I said "I have \(apples + oranges) pieces of fruit."
+"""
+
+var shoppingList = ["catfish", "water", "tulips"]
+shoppingList[1] = "bottle of water"
+
+shoppingList.append("beer")
+print("Shopping list: \(shoppingList)")
+
+let emptyArray: [String] = []
+let emptyDictionary: [String: Float] = [:]
+
+var occupations = [
+    "Malcolm": "Programmer",
+    "Kaylee": "Mechanic",
+    ]
+occupations["Ian"] = "Public Relations"
+
+shoppingList = []
+occupations = [:]
+
