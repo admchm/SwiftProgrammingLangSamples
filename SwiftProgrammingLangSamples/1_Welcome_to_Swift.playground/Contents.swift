5507ae7e0d6ed2d77fc765ae050fb847187b2229
Adam Chomicki <admchm@outlook.com>
2022-07-12 17:27:09 +0200

    Add spaces between sections. Finish functions and closures, and start working on objects and classes

--- SwiftProgrammingLangSamples/1_Welcome_to_Swift.playground/Contents.swift
+++ SwiftProgrammingLangSamples/1_Welcome_to_Swift.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import PlaygroundSupport
 
 /* A Swift Tour */
 print("Hello world")
@@ -12,6 +13,7 @@ let implicitInteger = 70
 let imiplicitDouble = 70.0
 let explicitDouble: Double = 70
 
+
 /* EXPERIMENT:
  * Create a constant with an explicit type of Float and a value of 4
  */
@@ -21,6 +23,7 @@ type(of: explicitFloat)
 let label = "The width is "
 let width = 94
 let widthLabel = label + String(width)
+
 
 /* EXPERIMENT:
  * Try removing the conversion to String from the last line. What error do you get?
@@ -32,6 +35,7 @@ let oranges = 5
 let appleSummary = "I have \(apples) apples."
 let fruitSummary = "I have \(apples + oranges) pieces of fruit."
 
+
 /* EXPERIMENT:
  * Use \() to include a floating-point calculation in a string and to include someone's name in a greeting.
  */
@@ -39,16 +43,19 @@ let yourName = "Adam"
 let appleDevicesCount: Float = 3.5
 let greetingWithCalc = "Hello \(yourName)! We want to remind you that you have \(appleDevicesCount) Apple devices - there is a half, because one of them is broken!"
 
+
 let quotation = """
 I said "I have \(apples) apples."
 And then I said "I have \(apples + oranges) pieces of fruit."
 """
+
 
 var shoppingList = ["catfish", "water", "tulips"]
 shoppingList[1] = "bottle of water"
 
 shoppingList.append("beer")
 print("Shopping list: \(shoppingList)")
+
 
 let emptyArray: [String] = []
 let emptyDictionary: [String: Float] = [:]
@@ -62,6 +69,7 @@ occupations["Ian"] = "Public Relations"
 shoppingList = []
 occupations = [:]
 
+
 /** CONTROL FLOW **/
 let indiualScores = [75, 43, 103, 87, 12]
 var teamScore = 0
@@ -74,9 +82,11 @@ for score in indiualScores {
 }
 print("Team score: \(teamScore)")
 
+
 var optionalString: String? = "Hello"
 print(optionalString == nil)
 // Prints "false"
+
 
 var optionalName: String? = "John Snow"
 var greeting = "Hello!"
@@ -84,6 +94,7 @@ var greeting = "Hello!"
 if let name = optionalName {
     greeting = "Hello, \(name)!"
 }
+
 
 /* EXPERIMENT:
  * Change optionalName to nil. What greeting do you get? Add an else clause that sets a different greeting if optionalName is nil.
@@ -96,6 +107,7 @@ if let name = experimentalOptionalName {
     greeting = "Hello, hmm..."
 }
 
+
 let nickname: String? = nil
 let fullName: String = "John Appleseed"
 let informalGreeting = "Hi \(nickname ?? fullName)"
@@ -103,6 +115,7 @@ let informalGreeting = "Hi \(nickname ?? fullName)"
 if let nickname {
     print("Hey, \(nickname)")
 }
+
 
 let vegetable = "red pepper"
 switch vegetable {
@@ -116,6 +129,7 @@ default:
     print("Everything tastes good in soup.")
 }
 
+
 /*
  * Try removing the default case. What error do you get?
  */
@@ -127,6 +141,7 @@ let interestingNumbers = [
     "Square": [1, 4, 9, 16, 25],
 ]
 
+
 var largest = 0
 for (_, numbers) in interestingNumbers {
     for number in numbers {
@@ -136,6 +151,7 @@ for (_, numbers) in interestingNumbers {
     }
 }
 print("largest = \(largest)")
+
 
 /* EXPERIMENT:
  * Replace the _ with a variable name, and keep track of which kind of number was the biggest
@@ -152,6 +168,7 @@ for (numberType, numbers) in interestingNumbers {
     }
     biggest = 0
 }
+
 
 print("Biggest numbers in each category = \(dictResults)")
 
@@ -172,12 +189,14 @@ for i in 0..<4 {
     total += i
 }
 
+
 /** FUNCTIONS AND CLOSURES **/
 func greet(person: String, day: String) -> String {
     return "Hello \(person), today is \(day)"
 }
 
 greet(person: "Mark", day: "Monday")
+
 
 /* EXPERIMENT:
  * Remove the day parameter. Add a parameter to include today's lunch special in the greeting.
@@ -188,8 +207,203 @@ func greeting(person: String, lunchSpecial: String) -> String {
 
 greeting(person: "Paul", lunchSpecial: "pizza")
 
+
 func newGreet(_ person: String, on day: String) -> String {
     return "Hello \(person), today is \(day)"
 }
 
 newGreet("Bart", on: "Friday")
+
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum) //
+print(statistics.2)   // these are the same
+
+
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+returnFifteen()
+
+
+// returning another function as its value
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
+
+
+// function can take another function as one of its arguments
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+
+var numbers = [3, 19, 17, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
+
+
+// closure
+numbers.map({ (number: Int) -> Int in
+    let result = 3 * number
+    return result
+})
+
+
+/* EXPERIMENT:
+ * Rewrite the closure to return zero for all odd numbers
+ */
+numbers.map({ ( number: Int) -> Int in
+    if number % 2 != 0 {
+        return 0
+    } else {
+        return number
+    }
+})
+
+
+// writing closures more concisely
+let mappedNumbers = numbers.map({ number in
+    10 * number })
+print(mappedNumbers)
+
+// refer to parameters by number instead of by name
+let sortedNumbers = numbers.sorted { $0 < $1}
+print(sortedNumbers)
+
+
+/** OBJECTS AND CLASSES **/
+
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+
+/* EXPERIMENT:
+ * Add a constant property with let, and add another method that takes an argument
+ */
+
+class Amount {
+    var value = 15
+    let exchangeRate = 3.887
+    
+    func simpleDescription() -> String {
+        return "The amount equals \(value)."
+    }
+    
+    func polishCurrencyCalculation() -> String {
+        return "You can exchange this amount in Poland to \(Double(value) * exchangeRate) of polish zloty."
+    }
+}
+
+
+// creating a class instance
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription()
+
+// class initializer to set up the class when an instance is created. Use init to create one
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides"
+    }
+}
+var namedShape = NamedShape(name: "star")
+
+
+// subclassing, overriding
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+let test = Square(sideLength: 5.2, name: "my test square")
+test.area()
+test.simpleDescription()
+
+
+/* EXPERIMENT:
+ * Make another subclass of NamedShape called Circle that takes a radius and a name as arguments to its initializer.
+ * Implement an area() and a simpleDescription()
+ */
+
+class Circle: NamedShape {
+    var radius: Double
+    
+    init(radius: Double, name: String) {
+        self.radius = radius
+        super.init(name: name)
+    }
+    
+    func area() -> Double {
+        return 3.14 * (radius * radius)
+    }
+    
+    override func simpleDescription() -> String {
+        return "A circle with radius of \(radius) and area that equals \(area())."
+    }
+}
+
+let circle = Circle(radius: 1.1, name: "our test circle")
+circle.simpleDescription()
+
+
+// getter and setter
+class EquilateralTriangle: NamedShape {
+    
+}
