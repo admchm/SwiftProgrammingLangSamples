40da5936309135b03d803e7c3a3f730f11bab6e5
Adam Chomicki <admchm@outlook.com>
2023-01-19 07:41:26 +0100

    Complete methods and subscripts chapters

--- SwiftProgrammingLangSamples/12_Methods.playground/Contents.swift
+++ SwiftProgrammingLangSamples/12_Methods.playground/Contents.swift
@@ -1,3 +1,165 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Methods are functions that are associated with a particular type.
+
+// INSTANCE METHODS
+class Counter {
+    var counter = 0
+    func increment() {
+        counter += 1
+    }
+    
+    func increment(by amount: Int) {
+        counter += amount
+    }
+    
+    func reset() {
+        counter = 0
+    }
+}
+
+let counter = Counter()
+counter.increment()
+counter.increment(by: 5)
+counter.reset()
+
+// THE SELF PROPERTY
+// self - equivalent to instance itself
+// We use the 'self' property to refer to the current instance
+// within its own instance methods.
+
+class OtherCounter {
+    var counter: Int = 0
+    
+    func increment() {
+        self.counter += 1
+    }
+}
+
+// But in practice, we don't need to write
+// 'self' very often. We need to write 'self' when a parameter name for an
+// instance method has the same name as property of that instance.
+struct Point {
+    var x = 0.0, y = 0.0
+    func isToTheRightOf(x: Double) -> Bool {
+        return self.x > x
+    }
+}
+
+let somePoint = Point(x: 4.0, y: 5.0)
+if somePoint.isToTheRightOf(x: 1.0) {
+    print("This point is to the right of the line where x == 1.0")
+}
+
+// MODIFYING VALUE TYPES FROM WITHIN INSTANCE METHODS
+// By default, the properties of a value type can’t be modified from within
+// its instance methods. However, if you need to modify the properties of your
+// structure or enumeration within a particular method, you can opt in to
+// mutating behavior for that method.
+
+struct OtherPoint {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        x += deltaX
+        y += deltaY
+    }
+}
+
+var otherPoint = OtherPoint(x: 1.0, y: 1.0)
+otherPoint.moveBy(x: 2.0, y: 3.0)
+print("The point is at (\(otherPoint.x), \(otherPoint.y)).")
+
+// ASSIGNING TO SELF WITHIN A MUTATING METHOD
+struct DifferentPoint {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        self = DifferentPoint(x: 100, y: 50)
+    }
+}
+
+var differentPoint = DifferentPoint()
+print("DifferentPoint is at (\(differentPoint.x), \(differentPoint.y)).")
+
+differentPoint.moveBy(x: 1, y: 2)
+print("DifferentPoint is at (\(differentPoint.x), \(differentPoint.y)).")
+
+// Mutating methods for enumerations can set the implicit self parameter to
+// be a different case from the same enumeration:
+
+enum TriStateSwitch {
+    case off, low, high
+    
+    mutating func next() {
+        switch self {
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+        }
+    }
+}
+
+var ovenLight = TriStateSwitch.low
+ovenLight.next()
+
+/** TYPE METHODS **/
+
+class SomeClass {
+    class func someTypeMethod() {
+        // type method implementation goes here
+    }
+}
+
+SomeClass.someTypeMethod()
+
+struct LevelTracker {
+    static var highestUnlockedLevel = 1
+    var currentLevel = 1
+    
+    static func unlock(_ level: Int) {
+        if level > highestUnlockedLevel {
+            highestUnlockedLevel = level
+        }
+    }
+    
+    static func isUnlocked(_ level: Int) -> Bool {
+        return level <= highestUnlockedLevel
+    }
+    
+    @discardableResult
+    mutating func advance(to level: Int) -> Bool {
+        if LevelTracker.isUnlocked(level) {
+            currentLevel = level
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Player {
+    var tracker = LevelTracker()
+    let playerName: String
+    
+    func complete(level: Int) {
+        LevelTracker.unlock(level + 1)
+        tracker.advance(to: level + 1)
+    }
+    
+    init(name: String) {
+        playerName = name
+    }
+}
+
+var player = Player(name: "Argyrios")
+player.complete(level: 1)
+print("highest unlocked level is now \(LevelTracker.highestUnlockedLevel)")
+
+player = Player(name: "Beto")
+if player.tracker.advance(to: 6) {
+    print("player is now on level 6")
+} else {
+    print("level 6 hasn't yet been unlocked")
+}
--- SwiftProgrammingLangSamples/13_Subscripts.playground/Contents.swift
+++ SwiftProgrammingLangSamples/13_Subscripts.playground/Contents.swift
@@ -1,3 +1,93 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Classes, structures, and enumerations can define subscripts,
+// which are shortcuts for accessing the member elements of a
+// collection, list, or sequence. We use subscripts to set and
+// retrieve values by index without needing separate methods for
+// setting and retrieval.
+
+// SUBSCRIPT SYNTAX
+/*
+subscript(index: Int) -> Int {
+    get {
+        // return an appropriate subscript value here
+    }
+    
+    set(newValue) {
+        // perform a suitable action here
+    }
+}
+*/
+// As with read-only computed properties, you can simplify the
+// declaration of a read-only subscript by removing the get keyword
+// and its braces:
+/*
+subscript(index: Int) -> Int {
+    // Return an appropriate subscript value here.
+}
+*/
+
+struct TimesTable {
+    let multiplier: Int
+    
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+
+let twoTimesTable = TimesTable(multiplier: 2)
+print(twoTimesTable[2])
+
+// SUBSCRIPT USAGE
+var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+numberOfLegs["bird"] = 2
+
+// SUBSCRIPT OPTIONS
+
+struct Matrix {
+    let rows: Int
+    let columns: Int
+    var grid: [Double]
+    
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    
+    func isIndexValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(isIndexValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(isIndexValid(row: row, column: column), "Index out of range")
+                        grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+var matrix = Matrix(rows: 2, columns: 2)
+
+matrix[1, 1] = 2
+// matrix[2, 1] = 4 // This triggers an assert, because [2, 2] is outside of the matrix bounds.
+
+// TYPE SUBSCRIPTS
+// We can also define subscripts that are called on the type itself.
+// To indicate that we are going to write a type subscript, we are writing
+// static keyword before the subscript keyword
+
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    static subscript(n: Int) -> Planet {
+        return Planet(rawValue: n)!
+    }
+}
+
+let mars = Planet[4]
+print(mars)
